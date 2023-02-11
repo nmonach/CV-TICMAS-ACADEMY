a6534c8cefc0d19c566a719af6922cb055929dd2
Nicolas Monach <nico.monach@hotmail.com>
2023-02-11 17:56:37 -0300

    ENTREGA FINAL CV TICMAS

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// MOSTRAR LA INFORMACIÓN
+
+function showAllAboutMe () {
+    document.getElementById('allAboutMe').style.display = 'block'
+}
+function showAllKnowledge () {
+    document.getElementById('allKnowledge').style.display = 'block'
+}
+function showAllExperience () {
+    document.getElementById('allExperience').style.display = 'block'
+}
+function showAllFormation () {
+    document.getElementById('allFormation').style.display = 'block'
+}
+function showAllContact () {
+    document.getElementById('allContact').style.display = 'block'
+}
+function showAllInfo () {
+    document.getElementById('allInfo').style.display = 'block'
+}
+
+// OCULTAR LA INFORMACIÓN
+
+function hideAllAboutMe () {
+    document.getElementById('allAboutMe').style.display = 'none'
+}
+function hideAllKnowledge () {
+    document.getElementById('allKnowledge').style.display = 'none'
+}
+function hideAllExperience () {
+    document.getElementById('allExperience').style.display = 'none'
+}
+function hideAllFormation () {
+    document.getElementById('allFormation').style.display = 'none'
+}
+function hideAllContact () {
+    document.getElementById('allContact').style.display = 'none'
+}
+function hideAllInfo () {
+    document.getElementById('allInfo').style.display = 'none'
+}
